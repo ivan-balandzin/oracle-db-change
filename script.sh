94614b3361adbcf009818c81fc667dd81ff8510f
Ivan Balandzin <ivan_balandzin@epam.com>
2016-05-03 13:35:17 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 cd ${WORKSPACE}
 out="$(git log --pretty=format:"%an"| tail -1)"
-sed -i "s/author: .*/author: $out 12345/g" database.changelog-master.json
+sed -i "s/author: .*/author: $out/g" database.changelog-master.json
